ad0922611ea7cc6ff72a6c7fa6c356ae9b25972c
KegangPeng <Kegangpeng1@gmail.com>
2025-01-14 11:01:56 -0800

    first testing

--- _IDE Hello World Test/Hello_World.cpp
+++ _IDE Hello World Test/Hello_World.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 // Just a simple hello world program to make sure the IDE is working
-int main() {
+int main()
+{
 
     cout << "Hello World!" << endl;
     cout << "It looks like your IDE is working fine!" << endl;
     cout << "Nice!" << endl;
-    
+
     return 0;
 }
