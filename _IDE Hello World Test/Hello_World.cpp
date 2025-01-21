ad95757afb00899146427df68aecb1ebe8d251ab
KegangPeng <Kegangpeng1@gmail.com>
2025-01-20 19:23:16 -0800

    testing Clion git commit 1/20/2025 7:23pm

--- _IDE Hello World Test/Hello_World.cpp
+++ _IDE Hello World Test/Hello_World.cpp
@@ -8,6 +8,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "It looks like your IDE is working fine!" << endl;
     cout << "Nice!" << endl;
-    cout << "testing! on Clion git commit from pc" << endl;
+    cout << "testing! on Clion git commit from pc 2.0 at 7:22pm " << endl;
     return 0;
 }
